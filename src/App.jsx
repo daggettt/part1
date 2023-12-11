4fe018c4b4e1147b1f73ed092f5cdeb3a4fce89a
rdaggett <rebeccaldaggett@gmail.com>
2023-12-11 08:12:24 -0500

    Submit solution for exercise 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
+/* Refactor to have 3 new components - Header, Content, Footer*/
 
-function Hello() {
-  console.log('this is a test from hello')
+const Header = (props) => {
+  console.log('this is the header component')
   return (
     <div>
-      <p>Hello again!</p>
+      <h1>{props.course}</h1>
     </div>
   )
 }
 
-function App() {
-  console.log('this is a test from App()')
+const Content = (props) => {
   return (
     <div>
-      <h1>Another greeting!</h1>
-      <Hello />
-      <Hello />
-      <Hello />
+      <h3>Unit: {props.part}</h3>
+      <p>Exercises: {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <h4>Total exercises for the course: {props.total}</h4>
+    </div>
+  )
+}
+
+const App = () => {
+  console.log('this is exercise 1.1')
+
+  const course = 'Half Stack application dev'
+  const part1 = 'Fundamentals of React'
+  const exercise1 = 10
+  const part2 = 'Using props to pass data'
+  const exercise2 = 7
+  const part3 = 'State of a component'
+  const exercise3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part={part1} exercises={exercise1}/>
+      <Content part={part2} exercises={exercise2}/>
+      <Content part={part3} exercises={exercise3}/>
+      <Total total={exercise1 + exercise2 + exercise3}/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import.meta.hot
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
