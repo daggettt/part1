aadff320c50c479f1e21ef9f885730b7d767e18f
rdaggett <rebeccaldaggett@gmail.com>
2023-12-12 17:26:30 -0500

    Complete exercise 1.4

--- src/App.jsx
+++ src/App.jsx
@@ -28,28 +28,32 @@ const Total = (props) => {
 const App = () => {
 
   console.log('1.3 complete')
+  console.log('1.4 complete')
+  console.log('1.5 ongoing')
 
-  const course = 'Half Stack application dev'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  }
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content name={part1.name} exercises={part1.exercises}/>
-      <Content name={part2.name} exercises={part2.exercises}/>
-      <Content name={part3.name} exercises={part3.exercises}/>
-      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content name={parts[0].name} exercises={parts[0].exercises}/>
+      <Content name={parts[1].name} exercises={parts[1].exercises}/>
+      <Content name={parts[2].name} exercises={parts[2].exercises}/>
+      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
     </div>
   )
 }
