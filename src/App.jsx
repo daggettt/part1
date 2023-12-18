d7a16e4850cb05dfc6b6330f4f84230ffd6151c0
rdaggett <rebeccaldaggett@gmail.com>
2023-12-18 18:04:25 -0500

    Create helper function for Hello component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 /* Part 1 C */
 
+
 const Hello = (props) => {
-  return (
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
+  return(
     <div>
       <p>
         Hello {props.name}, you are {props.age} years old.
+      </p>
+      <p>
+        So you were probably born in {bornYear()}.
       </p>
     </div>
   )
