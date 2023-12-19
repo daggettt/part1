257b2c3fc9b6b5d96d8553b59d50043ad837d599
rdaggett <rebeccaldaggett@gmail.com>
2023-12-18 19:03:57 -0500

    Add counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
 /* Part 1 C */
 
 
-const Hello = ({name, age}) => {
-  
-  console.log(name, age)
-
-  const bornYear = () => new Date().getFullYear() - age
-
-  return(
-    <div>
-      <p>
-        Hello {name}, you are {age} years old.
-      </p>
-      <p>
-        So you were probably born in {bornYear()}.
-      </p>
-    </div>
-  )
-}
-
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
+    <div>{counter}</div>
   )
 }
 
