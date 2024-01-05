6e2456d733b9d3e92e7e1a0228b0a4eabf99ec1f
rdaggett <rebeccaldaggett@gmail.com>
2024-01-05 11:30:12 -0500

    Practice destructuring.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 /* Part 1 C */
 
+const Hello = ({name, age}) => {
 
-const App = (props) => {
-  const {counter} = props
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
-    <div>{counter}</div>
+    <div>
+      <p>
+        Hello {name}, you are {age} years old.
+      </p>
+      <p>So you were probably born in {bornYear()}.</p>
+    </div>
   )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={20+8} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+
+
 }
 
 export default App
