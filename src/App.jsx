28eb40698e41991c8e44871412fe76528b15e322
rdaggett <rebeccaldaggett@gmail.com>
2024-01-05 15:12:34 -0500

    Add functionality to counter, add destructure display and button components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-/* Part 1 C - Stateful Components */
+/* Part 1 C - Complete */
 
 import { useState } from "react"
 
+const Display = ({counter}) => <div>{counter}</div>
+
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
 
-  const handleClick = () => {
-    setCounter(counter + 1)
-  }
- 
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={handleClick}>plus</button>
-      <button onClick={() => setCounter(0)}>Reset</button>
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text='Add 1'/>
+      <Button onClick={decreaseByOne} text='Subtract 1'/>
+      <Button onClick={setToZero} text='Reset'/>
     </div>
   )
 }
