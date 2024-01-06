0018cc2b2145b5d5047e3367f5aa6e2c5b41b0b1
rdaggett <rebeccaldaggett@gmail.com>
2024-01-06 17:21:51 -0500

    Add right and left buttons, and allClicks array.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
-/* Part 1 C - Complete */
+/* Part 1 D */
 
 import { useState } from "react"
 
-const Display = ({counter}) => <div>{counter}</div>
-
-
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
+  
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text='Add 1'/>
-      <Button onClick={decreaseByOne} text='Subtract 1'/>
-      <Button onClick={setToZero} text='Reset'/>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
+
 }
 
 export default App
